83cfab472c136feb130f5a69d0db45e319e788e7
GabrielReyesOrtiz <greyes17@ucol.mx>
2021-08-27 20:16:22 -0500

    Simulate race complete

--- race.js
+++ race.js
@@ -5,17 +5,20 @@ export default class Race {
     this._player2 = new Player(0);
   }
   run() {
+    let round = 0;
     do {
+      round++;
       let prob = this._player1.throwDice();
       this._player1.setPos(this._player1.getPos() + prob);
 
+      console.log(`Round ${round} PLayer1 position ${this._player1.getPos()} `);
+
       let prob2 = this._player2.throwDice();
       this._player2.setPos(this._player2.getPos() + prob2);
+      console.log(`Round ${round} PLayer2 position ${this._player2.getPos()} `);
     } while (this._player1.getPos() < 100 && this._player2.getPos() < 100);
   }
   win() {
-    console.log("PLayer1  " + this._player1.getPos());
-    console.log("PLayer2  " + this._player2.getPos());
     if (this._player1.getPos() >= 100 && this._player2.getPos() >= 100) {
       console.log("ES EMPATE");
     } else {
